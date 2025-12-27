5a808a35fd9424d03f341404114a9f91017ca35c
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-12-27 12:33:24 +0200

    vite.config.ts: set up alias and fix some bugs

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
+
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@modules": path.resolve(__dirname, "./src/modules"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@ui": path.resolve(__dirname, "./src/ui")
+    }
+  }
 })
