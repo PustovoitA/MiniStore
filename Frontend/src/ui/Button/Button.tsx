24b3f6b698f3906bdf3c404cfd1bfdf70addf2de
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-01-02 12:32:42 +0200

    Button.tsx: add type to Button

--- Frontend/src/ui/Button/Button.tsx
+++ Frontend/src/ui/Button/Button.tsx
@@ -2,12 +2,14 @@
 
 type ButtonProps = {
     value:string,
-    bgColor:string
+    bgColor:string,
+    type:"submit" | "reset" | "button" | undefined,
 }
 
-const Button = ({value, bgColor}:ButtonProps) => {
+const Button = ({value, bgColor, type}:ButtonProps) => {
     return(<>
         <button
+         type={type}
          className="text-white tracking-[2px] text-sm py-4.5 px-10.5 cursor-pointer font-[Jost]"
          style={{backgroundColor:bgColor}}
          >{value}</button>
