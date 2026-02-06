844770f2b6254763e83426086aa6deda65a2ff39
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-02-06 14:18:48 +0200

    List.tsx: base settings for header list

--- Frontend/src/modules/Header/components/List.tsx
+++ Frontend/src/modules/Header/components/List.tsx
@@ -1,7 +1,18 @@
 
 
 const List = () =>{
-    return(<></>)
+    return(<>
+        <ul className="flex gap-4 font-[Jost] cursor-pointer">
+            <li>HOME</li>
+            <li className="flex items-center">
+                PAGES
+                <span className="material-symbols-outlined">arrow_drop_down</span>
+            </li>
+            <li>ABOUT</li>
+            <li>BLOGS</li>
+            <li>CONTACT</li>
+        </ul>
+    </>)
 }
 
 export default List
