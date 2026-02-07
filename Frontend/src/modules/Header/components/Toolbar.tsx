46f2550dfac818bb7d6f9a6ff75955af3654056a
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-02-07 14:13:59 +0200

    Toolbar.tsx: add icons and chenge link to corekt

--- Frontend/src/modules/Header/components/Toolbar.tsx
+++ Frontend/src/modules/Header/components/Toolbar.tsx
@@ -1,6 +1,18 @@
 
 const Toolbar = () =>{
-    return(<></>)
+    return(<>
+        <ul>
+            <li>
+                <span className="material-symbols-outlined">search</span>
+            </li>
+            <li>
+                <span className="material-symbols-outlined">person</span>
+            </li>
+            <li>
+                <span className="material-symbols-outlined">shopping_cart</span>
+            </li>
+        </ul>
+    </>)
 }
 
 export default Toolbar
