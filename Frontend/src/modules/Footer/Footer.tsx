34f0af04bfcc2a3693ead5de981ffca746ef85ba
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-02-09 12:40:55 +0200

    create some components and import to footer

--- Frontend/src/modules/Footer/Footer.tsx
+++ Frontend/src/modules/Footer/Footer.tsx
@@ -1,4 +1,8 @@
-
+import { socialLinks } from "./components/socialLinks"
+import { quickLinks } from "./components/quickLinks"
+import { helpAndInfo } from "./components/helpAndInfo"
+import { contacts } from "./components/contacts"
+import { insta } from "./components/insta"
 
 const Footer = () => {
     return(<>footer is working</>)
