0b619288c9f7174b4734f6a47577abd51f71eb5f
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-02-10 13:56:06 +0200

    add one meta tad and SocialLinks.tsx: add html

--- Frontend/src/modules/Footer/components/socialLinks.tsx
+++ Frontend/src/modules/Footer/components/socialLinks.tsx
@@ -1,6 +1,47 @@
 
+import { Logo } from "@ui/Logo"
+
 const SocialLinks = () => {
-    return(<>SocialLinks</>)
+    return(<>
+    <Logo/>
+
+    <p>
+        Nisi, purus vitae, ultrices nunc. Sit ac sit suscipit hendrerit. 
+        Gravida massa volutpat aenean odio erat nullam fringilla.
+    </p>
+
+    <ul>
+        <li>
+            <a id="Facebook" href="#">
+                Facebook
+            </a>
+        </li>
+
+        <li>
+            <a id="Instagram" href="#">
+                Instagram
+            </a>
+        </li>
+        
+        <li>
+            <a id="Twiter" href="#">
+                Twiter
+            </a>
+        </li>
+
+        <li>
+            <a id="LinkedIn" href="#">
+                LinkedIn
+            </a>
+        </li>
+
+        <li>
+            <a id="YouTube" href="">
+                YouTube
+            </a>
+        </li>
+    </ul>
+    </>)
 }
 
 export default SocialLinks
