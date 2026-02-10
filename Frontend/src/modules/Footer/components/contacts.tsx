54a44c2dfe860195a05598fcd9c1a2831a55f904
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-02-10 22:57:37 +0200

    Contacts.tsx: add html

--- Frontend/src/modules/Footer/components/contacts.tsx
+++ Frontend/src/modules/Footer/components/contacts.tsx
@@ -1,6 +1,26 @@
 
 const Contacts = () => {
-    return(<>Contacts</>)
+    return(<div>
+        <h1>CONTACT US</h1>
+
+        <ul>
+            <li>
+                <p>
+                    Do you have any queries or
+                    suggestion?
+                </p>
+                <a href="#">yourinfo@gmail.com</a>
+            </li>
+
+            <li>
+                <p>
+                    If you need support? Just give us
+                    a call.
+                </p>
+                <a href="#">+55 111 222 333 44</a>
+            </li>
+        </ul>
+    </div>)
 }
 
 export default Contacts
