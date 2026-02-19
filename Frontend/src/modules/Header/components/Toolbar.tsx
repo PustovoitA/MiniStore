920adb12c4c0ac90cd87443df1b7d56c7fe0563d
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-02-19 10:27:51 +0200

    add transition-colors to header links and header toolbar

--- Frontend/src/modules/Header/components/Toolbar.tsx
+++ Frontend/src/modules/Header/components/Toolbar.tsx
@@ -2,13 +2,13 @@
 const Toolbar = () =>{
     return(<>
         <ul className="flex gap-4">
-            <li className="flex items-center hover:text-(--blue-color) cursor-pointer">
+            <li className="flex items-center hover:text-(--blue-color) cursor-pointer transition-colors">
                 <span className="material-symbols-outlined">search</span>
             </li>
-            <li className="flex items-center hover:text-(--blue-color) cursor-pointer">
+            <li className="flex items-center hover:text-(--blue-color) cursor-pointer transition-colors">
                 <span className="material-symbols-outlined">person</span>
             </li>
-            <li className="flex items-center hover:text-(--blue-color) cursor-pointer">
+            <li className="flex items-center hover:text-(--blue-color) cursor-pointer transition-colors">
                 <span className="material-symbols-outlined">shopping_cart</span>
                 <span className="flex items-center">(0)</span>
             </li>
