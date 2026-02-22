8c078a95c6b811bf3489448d9803974de6726f5c
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-02-22 21:38:22 +0200

    Insta.tsx: do the base html and css

--- Frontend/src/modules/Footer/components/insta.tsx
+++ Frontend/src/modules/Footer/components/insta.tsx
@@ -1,6 +1,26 @@
 
 const Insta = () => {
-    return(<>Insta</>)
+    return(<div className="flex flex-col items-center">
+        <h1 className="font-[Jost]">SHOP OUR INSTA</h1>
+
+        <ul className="flex w-full justify-between">
+            <li>
+                <img src="" alt="insta image" />
+            </li>
+            <li>
+                <img src="" alt="insta image" />
+            </li>
+            <li>
+                <img src="" alt="insta image" />
+            </li>
+            <li>
+                <img src="" alt="insta image" />
+            </li>
+            <li>
+                <img src="" alt="insta image" />
+            </li>
+        </ul>
+    </div>)
 }
 
 export default Insta
