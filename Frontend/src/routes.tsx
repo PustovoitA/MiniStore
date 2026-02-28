7c4572421b5509e73b59f8b1e9524eaa59f8241e
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-02-28 10:54:14 +0200

    stap route.tsx

---
+++ Frontend/src/routes.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import { Home } from "@pages/Home"
+
+const AppRoutes = () => {
+    const navigationRoutes = [
+        {
+            path: "Home",
+            element: <Home/>
+        }
+    ]
+
+    return <Routes>{navigationRoutes.map(route => 
+        <Route key={route.path} path={route.path} element={route.element}/>
+    )}</Routes>
+}
+
+export default AppRoutes
