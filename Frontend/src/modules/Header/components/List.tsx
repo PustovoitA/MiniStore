ec7764be1b0641c9ba413b705a1c6f7f787864e3
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-03-09 09:50:42 +0200

    List.tsx: add functional to change text color if page = list item

--- Frontend/src/modules/Header/components/List.tsx
+++ Frontend/src/modules/Header/components/List.tsx
@@ -1,20 +1,29 @@
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 
 
 const List = () =>{
     const navigate = useNavigate();
 
+    const location = useLocation();
 
     return(<>
         <ul className="flex gap-12 font-[Jost] text-black">
-            <li onClick={() => navigate("/")} className="flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest">HOME</li>
-            <li className="flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest">
+            <li
+            onClick={() => navigate("/")} 
+            className={`${location.pathname === "/" ? "text-[#72AEC8]" : "text-black"} flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest`}>HOME</li>
+            <li
+            className={`${location.pathname === "/Pages" ? "text-[#72AEC8]" : "text-black"} flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest`}>
                 PAGES
                 <span className="material-symbols-outlined">arrow_drop_down</span>
             </li>
-            <li onClick={() => navigate("About")} className="flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest">ABOUT</li>
-            <li onClick={() => navigate("Blog")} className="flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest">BLOGS</li>
-            <li className="flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest">CONTACT</li>
+            <li 
+            onClick={() => navigate("About")} 
+            className={`${location.pathname === "/About" ? "text-[#72AEC8]" : "text-black"} flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest`}>ABOUT</li>
+            <li
+            onClick={() => navigate("Blog")} 
+            className={`${location.pathname === "/Blog" ? "text-[#72AEC8]" : "text-black"} flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest`}>BLOGS</li>
+            <li
+            className={`${location.pathname === "/Contact" ? "text-[#72AEC8]" : "text-black"} flex items-center hover:text-(--blue-color) cursor-pointer transition-colors tracking-widest`}>CONTACT</li>
         </ul>
     </>)
 }
