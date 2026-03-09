aa01fdf5f377b1580e227b75a9ffd6cc67570a70
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-03-09 10:09:07 +0200

    quickLinks.tsx: add function scrolToTop and add Links to change page in section quick links

--- Frontend/src/modules/Footer/components/quickLinks.tsx
+++ Frontend/src/modules/Footer/components/quickLinks.tsx
@@ -1,23 +1,32 @@
+import { Link } from "react-router-dom"
 
 const QuickLinks = () => {
+
+    function scrolToTop(){
+        window.scrollTo({
+            top:0,
+            behavior: "smooth"
+        })
+    }
+
     return (<div className="flex flex-col gap-2">
         <h1 className="font-[Jost] text-xl tracking-wider">QUICK LINKS</h1>
 
         <ul className="flex flex-col gap-1 font-[Jost]">
-            <li className="hover:text-(--blue-color) transition-colors">
-                <a className="text-[15px] tracking-widest" href="#">HOME</a>
+            <li onClick={() => scrolToTop()} className="hover:text-(--blue-color) transition-colors">
+                <Link className="text-[15px] tracking-widest" to="/">HOME</Link>
             </li>
-            <li className="hover:text-(--blue-color) transition-colors">
-                <a className="text-[15px] tracking-widest" href="#">ABOUT</a>
+            <li onClick={() => scrolToTop()} className="hover:text-(--blue-color) transition-colors">
+                <Link className="text-[15px] tracking-widest" to="/About">ABOUT</Link>
             </li>
-            <li className="hover:text-(--blue-color) transition-colors">
-                <a className="text-[15px] tracking-widest" href="#">SHOP</a>
+            <li onClick={() => scrolToTop()} className="hover:text-(--blue-color) transition-colors">
+                <Link className="text-[15px] tracking-widest" to="#">SHOP</Link>
             </li>
-            <li className="hover:text-(--blue-color) transition-colors">
-                <a className="text-[15px] tracking-widest" href="#">BLOGS</a>
+            <li onClick={() => scrolToTop()} className="hover:text-(--blue-color) transition-colors">
+                <Link className="text-[15px] tracking-widest" to="/Blog">BLOGS</Link>
             </li>
-            <li className="hover:text-(--blue-color) transition-colors">
-                <a className="text-[15px] tracking-widest" href="#">CONTACT</a>
+            <li onClick={() => scrolToTop()} className="hover:text-(--blue-color) transition-colors">
+                <Link className="text-[15px] tracking-widest" to="/Contact">CONTACT</Link>
             </li>
         </ul>
     </div>)
