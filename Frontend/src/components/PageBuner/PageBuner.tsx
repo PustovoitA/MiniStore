6664e4fcf182c5955e99cde97eef9781432c2b36
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-03-11 20:59:10 +0200

    PageBuner.tsx:add some style to h1

--- Frontend/src/components/PageBuner/PageBuner.tsx
+++ Frontend/src/components/PageBuner/PageBuner.tsx
@@ -7,7 +7,7 @@ const PageBuner = () => {
     const getPathName = location.pathname
 
     return(<div className="flex items-center justify-center w-full h-87.5 bg-(--grey-light-color)">
-        <h1>{getPathName.slice(1)}</h1>
+        <h1 className="font-[Jost] text-8xl font-light">{getPathName.slice(1).toUpperCase()}</h1>
     </div>)
 }
 
