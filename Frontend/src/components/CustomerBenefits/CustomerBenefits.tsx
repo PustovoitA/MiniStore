46aa414d6d5f5f89dfe96bc0e681e7fdb1ebed2d
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-03-25 21:22:42 +0200

    CustomersBenefits.tsx: some chengint to center the commponent

--- Frontend/src/components/CustomerBenefits/CustomerBenefits.tsx
+++ Frontend/src/components/CustomerBenefits/CustomerBenefits.tsx
@@ -1,17 +1,22 @@
 
 const CustomerBenefits = () => {
-    return(<div className="flex items-center w-(--width)">
-        <div className="free_delivery">
-            <h1>FREE DELIVERY</h1>
-        </div>
-        <div className="quality_guarantee">
-            <h1>QUALITY GUARANTEE</h1>
-        </div>
-        <div className="daily_offers">
-            <h1>DAILY OFFERS</h1>
-        </div>
-        <div className="100%_secure_payment">
-            <h1>100% SECURE PAYMENT</h1>
+    return(<div className="flex items-center justify-center w-full">
+        <div className="flex items-center justify-between w-(--width)">
+            <div className="free_delivery">
+                <h1>FREE DELIVERY</h1>
+            </div>
+
+            <div className="quality_guarantee">
+                <h1>QUALITY GUARANTEE</h1>
+            </div>
+
+            <div className="daily_offers">
+                <h1>DAILY OFFERS</h1>
+            </div>
+
+            <div className="100%_secure_payment">
+                <h1>100% SECURE PAYMENT</h1>
+            </div>
         </div>
     </div>)
 }
