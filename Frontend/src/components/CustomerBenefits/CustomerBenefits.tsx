51dc22dd45efdfa0f3eb8b20928f5a4a4db6acb3
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-03-29 18:03:17 +0300

    CustomerBenefits.tsx: add icons

--- Frontend/src/components/CustomerBenefits/CustomerBenefits.tsx
+++ Frontend/src/components/CustomerBenefits/CustomerBenefits.tsx
@@ -3,22 +3,34 @@ const CustomerBenefits = () => {
     return(<div className="flex items-center justify-center w-full">
         <div className="flex items-center justify-between w-(--width)">
             <div className="free_delivery flex flex-col items-start">
-                <h1 className="text-xl">FREE DELIVERY</h1>
+                <div className="flex items-center gap-3">
+                    <span className="material-symbols-outlined text-(--blue-color)">shopping_cart</span>
+                    <h1 className="text-xl">FREE DELIVERY</h1>
+                </div>
                 <p className="text-(--grey-text-color) w-50">Consectetur adipi elit lorem ipsum dolor sit amet.</p>
             </div>
 
             <div className="quality_guarantee flex flex-col items-start">
-                <h1 className="text-xl">QUALITY GUARANTEE</h1>
+                <div className="flex items-center gap-3">
+                    <span className="material-symbols-outlined text-(--blue-color)">editor_choice</span>
+                    <h1 className="text-xl">QUALITY GUARANTEE</h1>
+                </div>
                 <p className="text-(--grey-text-color) w-50">Dolor sit amet orem ipsu mcons ectetur adipi elit.</p>
             </div>
 
             <div className="daily_offers flex flex-col items-start">
-                <h1 className="text-xl">DAILY OFFERS</h1>
+                <div className="flex items-center gap-3">
+                    <span className="material-symbols-outlined text-(--blue-color)">loyalty</span>
+                    <h1 className="text-xl">DAILY OFFERS</h1>
+                </div>
                 <p className="text-(--grey-text-color) w-50">Amet consectetur adipi elit loreme ipsum dolor sit.</p>
             </div>
 
             <div className="100%_secure_payment flex flex-col items-start">
-                <h1 className="text-xl">100% SECURE PAYMENT</h1>
+                <div className="flex items-center gap-3">
+                    <span className="material-symbols-outlined text-(--blue-color)">verified_user</span>
+                    <h1 className="text-xl">100% SECURE PAYMENT</h1>
+                </div>
                 <p className="text-(--grey-text-color) w-50">Rem Lopsum dolor sit amet, consectetur adipi elit.</p>
             </div>
         </div>
