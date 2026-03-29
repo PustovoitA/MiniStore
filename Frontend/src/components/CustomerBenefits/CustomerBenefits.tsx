90c4ff18f0afe5acb203da2754e0a60be404c8fa
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-03-29 18:05:40 +0300

    CustumerBenefits.tsx: replase text

--- Frontend/src/components/CustomerBenefits/CustomerBenefits.tsx
+++ Frontend/src/components/CustomerBenefits/CustomerBenefits.tsx
@@ -7,7 +7,7 @@ const CustomerBenefits = () => {
                     <span className="material-symbols-outlined text-(--blue-color)">shopping_cart</span>
                     <h1 className="text-xl">FREE DELIVERY</h1>
                 </div>
-                <p className="text-(--grey-text-color) w-50">Consectetur adipi elit lorem ipsum dolor sit amet.</p>
+                <p className="text-(--grey-text-color) w-50 relative left-8.75">Consectetur adipi elit lorem ipsum dolor sit amet.</p>
             </div>
 
             <div className="quality_guarantee flex flex-col items-start">
@@ -15,7 +15,7 @@ const CustomerBenefits = () => {
                     <span className="material-symbols-outlined text-(--blue-color)">editor_choice</span>
                     <h1 className="text-xl">QUALITY GUARANTEE</h1>
                 </div>
-                <p className="text-(--grey-text-color) w-50">Dolor sit amet orem ipsu mcons ectetur adipi elit.</p>
+                <p className="text-(--grey-text-color) w-50 relative left-8.75">Dolor sit amet orem ipsu mcons ectetur adipi elit.</p>
             </div>
 
             <div className="daily_offers flex flex-col items-start">
@@ -23,7 +23,7 @@ const CustomerBenefits = () => {
                     <span className="material-symbols-outlined text-(--blue-color)">loyalty</span>
                     <h1 className="text-xl">DAILY OFFERS</h1>
                 </div>
-                <p className="text-(--grey-text-color) w-50">Amet consectetur adipi elit loreme ipsum dolor sit.</p>
+                <p className="text-(--grey-text-color) w-50 relative left-8.75">Amet consectetur adipi elit loreme ipsum dolor sit.</p>
             </div>
 
             <div className="100%_secure_payment flex flex-col items-start">
@@ -31,7 +31,7 @@ const CustomerBenefits = () => {
                     <span className="material-symbols-outlined text-(--blue-color)">verified_user</span>
                     <h1 className="text-xl">100% SECURE PAYMENT</h1>
                 </div>
-                <p className="text-(--grey-text-color) w-50">Rem Lopsum dolor sit amet, consectetur adipi elit.</p>
+                <p className="text-(--grey-text-color) w-50 relative left-8.75">Rem Lopsum dolor sit amet, consectetur adipi elit.</p>
             </div>
         </div>
     </div>)
