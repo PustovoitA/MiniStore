04cbdcf873cad0e4c82db7557874b520a24709d4
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-04-07 16:21:28 +0300

    HomeProductZone.tsx: add base html and css

--- Frontend/src/modules/HomeProductZone/HomeProductZone.tsx
+++ Frontend/src/modules/HomeProductZone/HomeProductZone.tsx
@@ -1,6 +1,18 @@
 
 const HomeProductZone = () => {
-    return(<></>)
+    return(<div className="flex flex-col justify-center items-center w-full">
+        <section className="mobile_sec w-(--width)">
+            <div className="title"></div>
+            <div className="products"></div>
+            <div className="switcher"></div>
+        </section>
+
+        <section className="watches_sec w-(--width)">
+            <div className="title"></div>
+            <div className="products"></div>
+            <div className="switcher"></div>
+        </section>
+    </div>)
 }
 
 export default HomeProductZone;
