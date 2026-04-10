7ccae83500ca5bce17f239862224e3afdca86b21
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-04-10 21:46:39 +0300

    HomeProductZone.tsx: dublicate mobile products to smart watches

--- Frontend/src/modules/HomeProductZone/HomeProductZone.tsx
+++ Frontend/src/modules/HomeProductZone/HomeProductZone.tsx
@@ -4,7 +4,7 @@ import ProductStore from "../store/ProductsStore";
 const HomeProductZone = () => {
     const products = ProductStore((state) => state.products);
 
-    return(<div className="flex flex-col justify-center items-center w-full">
+    return(<div className="flex flex-col gap-20 justify-center items-center w-full">
         <section className="mobile_sec flex flex-col items-center gap-5 w-(--width)">
             <div className="title flex items-center justify-between w-full font-[Jost]">
                 <h1 className="text-3xl">MOBILE PRODUCTS</h1>
@@ -19,10 +19,18 @@ const HomeProductZone = () => {
             <div className="switcher w-full"></div>
         </section>
 
-        <section className="watches_sec w-(--width)">
-            <div className="title"></div>
-            <div className="products"></div>
-            <div className="switcher"></div>
+        <section className="watches_sec flex flex-col items-center gap-5 w-(--width)">
+            <div className="title flex items-center justify-between w-full font-[Jost]">
+                <h1 className="text-3xl">SMART WATCHES</h1>
+                <div>
+                    <a className="text-[14px]" href="/Shop">GO TO SHOP</a>
+                    <hr className="h-0.5 bg-[#c8c8c8] border-none" />
+                </div>
+            </div>
+            <div className="flex items-center gap-4.5 w-full products">
+                {products.map(el => <ProductCart productPrice={el.price} productImageSrc={el.image} productName={el.name} countProductsInRow={5}/>)}
+            </div>
+            <div className="switcher w-full"></div>
         </section>
     </div>)
 }
