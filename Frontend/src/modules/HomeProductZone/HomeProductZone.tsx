e0a6c09d963c6d156a2e9b7c96add6951eac1cc6
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-04-10 12:41:00 +0300

    HomeProductZone.tsx: add some styles

--- Frontend/src/modules/HomeProductZone/HomeProductZone.tsx
+++ Frontend/src/modules/HomeProductZone/HomeProductZone.tsx
@@ -5,15 +5,18 @@ const HomeProductZone = () => {
     const products = ProductStore((state) => state.products);
 
     return(<div className="flex flex-col justify-center items-center w-full">
-        <section className="mobile_sec w-(--width)">
-            <div className="title">
-                <h1>MOBILE PRODUCTS</h1>
-                <a href="#">GO TO SHOP</a>
+        <section className="mobile_sec flex flex-col items-center gap-5 w-(--width)">
+            <div className="title flex items-center justify-between w-full font-[Jost]">
+                <h1 className="text-2xl">MOBILE PRODUCTS</h1>
+                <div>
+                    <a className="text-[14px])" href="#">GO TO SHOP</a>
+                    <span className=""></span>
+                </div>
             </div>
-            <div className="flex items-center gap-4.5 products">
+            <div className="flex items-center gap-4.5 w-full products">
                 {products.map(el => <ProductCart productPrice={el.price} productImageSrc={el.image} productName={el.name} countProductsInRow={5}/>)}
             </div>
-            <div className="switcher"></div>
+            <div className="switcher w-full"></div>
         </section>
 
         <section className="watches_sec w-(--width)">
