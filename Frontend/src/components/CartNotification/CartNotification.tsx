852d0b4c3b2df782a8e41df5581cfc532ab581d9
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-06-09 14:53:55 +0300

    CartNotification.tsx: added a condition that processes the message value and substitutes the context

--- Frontend/src/components/CartNotification/CartNotification.tsx
+++ Frontend/src/components/CartNotification/CartNotification.tsx
@@ -6,6 +6,17 @@ import styles from "./CartNotification.module.css";
 const CartNotification = () => {
     const notification = CartStore((state) => state.notification);
     const setNotification = CartStore((state) => state.setNotification);
+    const context = notification.value === "success"
+    ?{
+        massage: "PRODUCT ADDED TO CART",
+        bgCol: "#04bd04",
+        icon: "check_circle"
+    }
+    :{
+        massage: "ERROR PLEASE REPEAT LATER",
+        bgCol: "#e81e1e",
+        icon: "cancel"
+    }
 
     useEffect(()=>{
         if(!notification.open) return;
@@ -23,9 +34,9 @@ const CartNotification = () => {
     if(!notification.open) return null
 
     if(notification.open){
-        return(<div key={Date.now()} className={styles.notification} >
-            <span className="material-symbols-outlined text-[18px]">check_circle</span>
-            <p>PRODUCT ADDED TO CART</p>
+    return(<div key={Date.now()} className={`${styles.notification} bg-[${context.bgCol}]`} >
+            <span className="material-symbols-outlined text-[18px]">{context.icon}</span>
+            <p>{context.massage}</p>
         </div>)}
     }
 
