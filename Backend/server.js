6e03cdf2ec2a510fbbb4c5516d407d8507b2dec3
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-06-25 22:08:15 +0300

    retern to new version json-server

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults({ static: false })
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
