cf439c1a6487d7b6ad2260becc52dba9f5d2d2fa
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-07-03 21:52:09 +0300

    add adaptive to baners

--- Frontend/src/components/CustomerBenefits/CustomerBenefits.tsx
+++ Frontend/src/components/CustomerBenefits/CustomerBenefits.tsx
@@ -1,40 +1,47 @@
 
 const CustomerBenefits = () => {
-    return(<div className="flex items-center justify-center w-full">
-        <div className="flex items-center justify-between w-(--width)">
-            <div className="free_delivery flex flex-col items-start">
-                <div className="flex items-center gap-3">
-                    <span className="material-symbols-outlined text-(--blue-color) unselectable">shopping_cart</span>
-                    <h1 className="text-xl">FREE DELIVERY</h1>
-                </div>
-                <p className="text-(--grey-text-color) w-50 relative left-8.75">Consectetur adipi elit lorem ipsum dolor sit amet.</p>
-            </div>
+    const benefits = [
+        {
+            icon: "shopping_cart",
+            title: "FREE DELIVERY",
+            text: "Consectetur adipi elit lorem ipsum dolor sit amet.",
+        },
+        {
+            icon: "editor_choice",
+            title: "QUALITY GUARANTEE",
+            text: "Dolor sit amet orem ipsu mcons ectetur adipi elit.",
+        },
+        {
+            icon: "loyalty",
+            title: "DAILY OFFERS",
+            text: "Amet consectetur adipi elit loreme ipsum dolor sit.",
+        },
+        {
+            icon: "verified_user",
+            title: "100% SECURE PAYMENT",
+            text: "Rem Lopsum dolor sit amet, consectetur adipi elit.",
+        },
+    ];
 
-            <div className="quality_guarantee flex flex-col items-start">
-                <div className="flex items-center gap-3">
-                    <span className="material-symbols-outlined text-(--blue-color) unselectable">editor_choice</span>
-                    <h1 className="text-xl">QUALITY GUARANTEE</h1>
-                </div>
-                <p className="text-(--grey-text-color) w-50 relative left-8.75">Dolor sit amet orem ipsu mcons ectetur adipi elit.</p>
-            </div>
-
-            <div className="daily_offers flex flex-col items-start">
-                <div className="flex items-center gap-3">
-                    <span className="material-symbols-outlined text-(--blue-color) unselectable">loyalty</span>
-                    <h1 className="text-xl">DAILY OFFERS</h1>
-                </div>
-                <p className="text-(--grey-text-color) w-50 relative left-8.75">Amet consectetur adipi elit loreme ipsum dolor sit.</p>
-            </div>
-
-            <div className="100%_secure_payment flex flex-col items-start">
-                <div className="flex items-center gap-3">
-                    <span className="material-symbols-outlined text-(--blue-color) unselectable">verified_user</span>
-                    <h1 className="text-xl">100% SECURE PAYMENT</h1>
-                </div>
-                <p className="text-(--grey-text-color) w-50 relative left-8.75">Rem Lopsum dolor sit amet, consectetur adipi elit.</p>
+    return (
+        <div className="flex items-center justify-center w-full px-4 sm:px-8 lg:px-0">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-10 w-full max-w-(--width)">
+                {benefits.map(({ icon, title, text }) => (
+                    <div key={title} className="flex flex-col items-center sm:items-start text-center sm:text-left">
+                        <div className="flex items-center gap-3">
+                            <span className="material-symbols-outlined text-(--blue-color) unselectable">
+                                {icon}
+                            </span>
+                            <h1 className="text-lg lg:text-xl">{title}</h1>
+                        </div>
+                        <p className="text-(--grey-text-color) text-sm lg:text-base max-w-70 sm:pl-8.75 mt-1">
+                            {text}
+                        </p>
+                    </div>
+                ))}
             </div>
         </div>
-    </div>)
-}
+    );
+};
 
-export default CustomerBenefits
+export default CustomerBenefits;
