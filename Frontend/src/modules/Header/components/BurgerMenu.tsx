161ac7f5da90c1b0f6b7e9e010772bcf5b62e723
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-07-04 12:20:45 +0300

    adaptive header

--- Frontend/src/modules/Header/components/BurgerMenu.tsx
+++ Frontend/src/modules/Header/components/BurgerMenu.tsx
@@ -1,7 +1,9 @@
 
 
 const BurgerMenu = () => {
-    return(<></>)
+    return(<div className="flex items-center justify-center sm:hidden">
+        <span className="material-symbols-outlined cursor-pointer">menu</span>
+    </div>)
 }
 
 export default BurgerMenu
