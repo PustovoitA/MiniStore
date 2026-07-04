945e85a8d79ce35e095a85e3f7d185162a4dd7c9
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-07-04 16:52:09 +0300

    add closing and opening meno

---
+++ Frontend/src/assets/blockScroling.ts
@@ -0,0 +1,7 @@
+export function blockScroling () {
+    document.body.style.overflow = "hidden";
+}
+
+export function unblockScroling () {
+    document.body.style.overflow = "";
+}
--- Frontend/src/modules/Header/components/BurgerMenu.tsx
+++ Frontend/src/modules/Header/components/BurgerMenu.tsx
@@ -1,12 +1,35 @@
+import { blockScroling, unblockScroling } from "@/assets/blockScroling";
+import { useState } from "react"
 
 
 const BurgerMenu = () => {
-    return(<div className="flex items-center justify-center sm:hidden">
-        <span className="material-symbols-outlined cursor-pointer">menu</span>
+    const [menuIsOpen, setMenuValue] = useState(false)
 
-        <div className="w-full h-full absolute z-20 top-0 right-0 bg-[#00000074] backdrop-blur-md">
-            <div className="bg-[#fff] h-full w-[50%] absolute right-0 top-0 rounded-tl-2xl rounded-bl-2xl">
-                <span className="material-symbols-outlined cursor-pointer absolute top-2 left-2">close</span>
+    const openMenu = () => {
+        setMenuValue(true);
+        blockScroling();
+    }
+    const closeMenu = () => {
+        setMenuValue(false);
+        unblockScroling();
+    }
+
+    return (<div className="flex items-center justify-center sm:hidden">
+        <span onClick={() => openMenu()} className="material-symbols-outlined cursor-pointer">menu</span>
+
+        <div
+            className={`w-full h-full fixed z-20 top-0 right-0 bg-[#00000074] backdrop-blur-md transition-opacity duration-300 ${
+                menuIsOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+            }`}
+            onClick={() => closeMenu()}
+        >
+            <div
+                className={`bg-[#fff] h-full w-[50%] absolute right-0 top-0 rounded-tl-2xl rounded-bl-2xl transition-transform duration-300 ease-in-out ${
+                    menuIsOpen ? "translate-x-0" : "translate-x-full"
+                }`}
+                onClick={(e) => e.stopPropagation()}
+            >
+                <span onClick={() => closeMenu()} className="material-symbols-outlined cursor-pointer absolute top-2 left-2">close</span>
             </div>
         </div>
     </div>)
