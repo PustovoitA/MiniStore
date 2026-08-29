e2158fb7f0a89ece91e2d07ee72108e2a83cf38a
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-08-29 13:55:13 +0300

    add navigation to home page on click on logo

--- Frontend/src/ui/Logo/Logo.tsx
+++ Frontend/src/ui/Logo/Logo.tsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom"
 
 const Logo = () => {
+    const navigation = useNavigate();
     return(<>
-        <h1 className="font-[Jost] text-3xl sm:text-2xl md:text-3xl">MiniStore<span className="text-(--blue-color)">.</span></h1>
+        <h1
+        onClick={() => {navigation("/Home")}}
+        className="font-[Jost] text-3xl sm:text-2xl md:text-3xl cursor-pointer">
+            MiniStore
+            <span className="text-(--blue-color)">.</span>
+        </h1>
     </>)
 }
 
