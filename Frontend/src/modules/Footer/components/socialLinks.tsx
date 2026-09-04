21a62191c06c6a245a68311cb8167aecb6b6a73b
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-09-04 12:03:43 +0300

    change stroke-width to strokeWidth

--- Frontend/src/modules/Footer/components/socialLinks.tsx
+++ Frontend/src/modules/Footer/components/socialLinks.tsx
@@ -28,8 +28,8 @@ const SocialLinks = () => {
                 fill="none" 
                 xmlns="http://www.w3.org/2000/svg"
                 >
-                <rect x="3" y="3" width="18" height="18" rx="5" stroke="currentColor" stroke-width="2"/>
-                <circle cx="12" cy="12" r="4" stroke="currentColor" stroke-width="2"/>
+                <rect x="3" y="3" width="18" height="18" rx="5" stroke="currentColor" strokeWidth="2"/>
+                <circle cx="12" cy="12" r="4" stroke="currentColor" strokeWidth="2"/>
                 <circle cx="17" cy="7" r="1.5" fill="currentColor"/>
                 </svg>
             </a>
