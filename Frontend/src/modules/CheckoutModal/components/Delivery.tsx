8a5ccea847ce7a533323244826584bfdef8323ad
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-09-15 18:11:14 +0300

    add hover effect and selection effect in delivery component

--- Frontend/src/modules/CheckoutModal/components/Delivery.tsx
+++ Frontend/src/modules/CheckoutModal/components/Delivery.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react"
+
+type deliveryOptionType = "In-store pickup" | "Nova Poshta" | "Ukrposhta"
 
 const Delivery = () => {
+    const [deliveryOption, setDeliveryOption] = useState<deliveryOptionType>("In-store pickup")
+    
     return(<>
         <div
         style={{boxShadow: "0 0 10px 0 rgb(0 0 0 / 0.2)"}}
@@ -7,18 +12,24 @@ const Delivery = () => {
             <h1 className="text-[25px]">Delivery</h1>
             <ul className="flex items-center gap-2.5">
                 <li
-                className="flex flex-col justify-center items-center border border-(--border) rounded-md cursor-pointer px-[15px] py-[5px]">
-                    <span className="text-[20px]">In-store pickup</span>
+                onClick={() => { setDeliveryOption("In-store pickup") }}
+                className="group flex flex-col justify-center items-center border border-(--border) rounded-md cursor-pointer px-[15px] py-[5px] ">
+                    <span
+                    className={`${deliveryOption === "In-store pickup" ? "text-(--blue-color)" : "text-black"} group-hover:text-(--blue-color) text-[20px] transition-colors`}>In-store pickup</span>
                     <span className="text-(--grey-text-color) text-[14px]">payment upon receipt</span>
                 </li>
                 <li
-                className="flex flex-col justify-center items-center border border-(--border) rounded-md cursor-pointer px-[15px] py-[5px]">
-                    <span className="text-[20px]">Nova Poshta</span>
+                onClick={() => { setDeliveryOption("Nova Poshta") }}
+                className="group flex flex-col justify-center items-center border border-(--border) rounded-md cursor-pointer px-[15px] py-[5px]">
+                    <span 
+                    className={`${deliveryOption === "Nova Poshta" ? "text-(--blue-color)" : "text-black"} group-hover:text-(--blue-color) text-[20px] transition-colors`}>Nova Poshta</span>
                     <span className="text-(--grey-text-color) text-[14px]">extensive network</span>
                 </li>
                 <li
-                className="flex flex-col justify-center items-center border border-(--border) rounded-md cursor-pointer px-[15px] py-[5px]">
-                    <span className="text-[20px]">Ukrposhta</span>
+                onClick={() => { setDeliveryOption("Ukrposhta") }}
+                className="group flex flex-col justify-center items-center border border-(--border) rounded-md cursor-pointer px-[15px] py-[5px]">
+                    <span
+                    className={`${deliveryOption === "Ukrposhta" ? "text-(--blue-color)" : "text-black"} group-hover:text-(--blue-color) text-[20px] transition-colors`}>Ukrposhta</span>
                     <span className="text-(--grey-text-color) text-[14px]">affordable terms</span>
                 </li>
             </ul>
